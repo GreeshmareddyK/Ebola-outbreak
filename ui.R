398d71f06ac54bd72d8c56647cba3224413fe0b2
GreeshmareddyK <greeshma.kothireddy@gmail.com>
2016-09-02 10:00:49 -0400

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,47 @@
+require(googleVis)
+shinyServer(
+   
+     
+     pageWithSidebar(
+       headerPanel("Visualisation of Monthly Statistics of Ebola outbreak  "),
+        sidebarPanel(sliderInput("Year", "Election year to be displayed:", 
+                min=as.Date("2015-06-15"), max=as.Date("2015-12-12"), value = as.Date("2015-06-15"),step=30,
+                format="%F",animate=TRUE),
+              selectInput("type","Please select the type of data",choices= c("Total Deaths","Total Cases")), width=2
+      ),
+    
+mainPanel(
+
+                 
+
+                     tabsetPanel(
+
+                  tabPanel("Ebola Outbreak",
+
+                  fluidRow(
+
+
+
+
+                   column(6,htmlOutput("Plot")),
+
+                   column(4,htmlOutput("Plot2")) 
+
+                  ) 
+
+                  ),
+
+                   tabPanel("Word Cloud",fluidRow(column(6,offset=3,plotOutput("Plot3"))))                 
+
+                  )
+
+
+
+      
+
+   
+
+        )
+
+)
+)
